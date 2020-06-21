41c99ae811139ae07208819632b7a580122ef992
Jean Roux <jeanp.roux@gmail.com>
2020-06-21 15:33:55 +0200

    1. added babel \n 2. Added css handling in webpack \n 3. Added iamge handling in webpack

--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -1,4 +1,5 @@
-const sum = require('./sum');
+import sum from './sum';  // There is an export available, thus assigning it to a variable to use
+import './image_viewer';  // doesn't export any code so this syntax just call the js and it runs
 
 const total = sum(1,2);
 console.log(total);
